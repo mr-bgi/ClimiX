93d225e3a156918846eb33c2167e5aa8e892015a
mr-bgi <tola44356@gmail.com>
2024-07-23 08:42:31 +0700

    test vervel

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("headbar").style.boxShadow = "1px 1px 10px 1px rgba(0, 0, 0, 0.1)";
+        // document.getElementById("headbar").style.boxShadow = "1px 1px 10px 1px rgba(0, 0, 0, 0.1)";
         document.getElementById("logo").style.maxWidth = "150px";
         document.getElementById("headbar").style.background = "white";
         document.getElementById("searchBar").style.background = "white";
@@ -10,7 +10,7 @@ function scrollFunction() {
         document.getElementById("logo").style.maxWidth = "180px";
         document.getElementById("headbar").style.background = "transparent";
         document.getElementById("searchBar").style.background = "transparent";
-        document.getElementById("headbar").style.boxShadow = "1px 1px 10px 1px transparent";
+        // document.getElementById("headbar").style.boxShadow = "1px 1px 10px 1px transparent";
     }
 }
 
